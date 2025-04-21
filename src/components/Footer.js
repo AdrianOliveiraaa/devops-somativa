27ffa6ec595a1d2f279a834040222049059de818
Adrian Oliveira <o.adrian@pucpr.edu.br>
2025-04-21 01:33:01 -0300

    Adicionar Rodapé

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ marginTop: '2rem', textAlign: 'center', fontSize: '0.9rem', color: '#888' }}>
+      © 2025 - Somativa DevOps - Adrian Oliveira
+    </footer>
+  );
+};
+
+export default Footer;
