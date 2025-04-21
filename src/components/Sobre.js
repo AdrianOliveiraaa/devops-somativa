b81a37b109ff44f0ca1458da1c3542c754e66748
Adrian Oliveira <o.adrian@pucpr.edu.br>
2025-04-21 01:55:34 -0300

    Ajustes e alteração da página principal

---
+++ src/components/Sobre.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Sobre() {
+  return (
+    <section style={{ padding: '1rem', textAlign: 'center' }}>
+      <p>
+        Projeto criado para o estudos da disciplina DevOps.
+      </p>
+    </section>
+  );
+}
+
+export default Sobre;
